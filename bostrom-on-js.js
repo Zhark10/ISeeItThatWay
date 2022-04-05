880674a5541bd548465f961371744df80ed46a9a
Zhark10 <verygoodman18@yandex.ru>
2022-04-06 02:06:47 +0300

    GOD`S THOUGHTS

--- bostrom-on-js.js
+++ bostrom-on-js.js
@@ -2,3 +2,24 @@
 const createWorld = async () => {
   //...
 };
+
+// 2. GOD`S THOUGHTS
+const GodSThoughts = {
+  lifetimeRange: {minYears: 1, maxYears: 9999999999},
+  simulationsCount: 10,
+  getPlannedSimulations: function () {
+    const {
+      lifetimeRange: {minYears, maxYears},
+      simulationsCount,
+    } = this;
+    const simulations = new Array(simulationsCount).fill(null);
+    const setInitialSimulationInfo = () => ({
+      lifetime: Math.random() * (maxYears - minYears) + minYears,
+      ...this,
+    });
+    return simulations.map(setInitialSimulationInfo);
+  },
+  turnOffSimulation: async function () {},
+  analyze: async function () {},
+  //...
+};
