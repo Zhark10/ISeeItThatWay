78049d71d019121fe42e070b1b33c016a8afff7a
Zhark10 <verygoodman18@yandex.ru>
2022-04-07 21:31:46 +0300

    logic of world creation (started)

---
+++ modules/1_LogicOfWorldCreation.mjs
@@ -0,0 +1,27 @@
+  // 1. UNKNOWN LOGIC OF WORLD CREATION.
+  export const createWorld = async () => {
+    //...
+    const initialize = async _GodSHiddenThoughts => {
+      const generatedPresets = null
+      return generatedPresets
+    }
+
+    const bigBang = async _generatedPresets => {
+      const entities = null
+      return entities
+    }
+
+    const toDevelopCivilization = async _entities => {
+      const output = {
+        populationIsBroken: Boolean(Math.round(Math.random())),
+        destroyTheWorld: null,
+        rethinking: async _thoughts => _thoughts,
+      }
+      return output
+    }
+    return {
+      initialize,
+      bigBang,
+      toDevelopCivilization,
+    }
+  };
