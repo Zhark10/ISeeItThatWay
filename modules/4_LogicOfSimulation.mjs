0224a6fcdc978a34efbaa93bb9a91e604977a71e
Zhark10 <verygoodman18@yandex.ru>
2022-04-07 21:32:57 +0300

    run simulation func (started)

---
+++ modules/4_LogicOfSimulation.mjs
@@ -0,0 +1,27 @@
+import {createWorld} from './1_LogicOfWorldCreation.mjs'
+import {startNewSimulations} from './3_StartNewSimulations.mjs'
+
+// 4. HOW THE SIMULATIONS WORKS.
+export async function runSimulation(GodSHiddenThoughts) {
+  const statistics = { currentSimulation: null, generatedChildSimulations: null };
+
+  const currentWorld = await createWorld();
+  const generatedPresets = await currentWorld.initialize(GodSHiddenThoughts);
+  const entities = await currentWorld.bigBang(generatedPresets);
+  const output = await currentWorld.toDevelopCivilization(entities);
+  statistics.currentSimulation = output;
+
+  if (output.populationIsBroken) {
+    const selectedDestroyMethod =
+      output.destroyTheWorld || GodSHiddenThoughts.turnOffSimulation;
+    selectedDestroyMethod(currentWorld);
+  } else {
+    const coppiedGodSThoughts = { ...GodSHiddenThoughts, isFirstGod: false };
+    const thoughtsOfTheNewGod = await output.rethinking(coppiedGodSThoughts);
+    if (GodSHiddenThoughts.isFirstGod) {
+      const childSimulationsInfo = await startNewSimulations(thoughtsOfTheNewGod);
+      statistics.generatedChildSimulations = childSimulationsInfo;
+    }
+  }
+  return statistics;
+}
