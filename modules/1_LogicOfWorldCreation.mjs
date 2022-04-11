d782606b864b4155c62d1e345ba5e2ec5adfb541
Zhark10 <verygoodman18@yandex.ru>
2022-04-12 00:57:35 +0300

    func to class (world creation)

--- modules/1_LogicOfWorldCreation.mjs
+++ modules/1_LogicOfWorldCreation.mjs
@@ -1,27 +1,24 @@
-  // 1. UNKNOWN LOGIC OF WORLD CREATION.
-  export const createWorld = async () => {
-    //...
-    const initialize = async _GodSHiddenThoughts => {
-      const generatedPresets = null
-      return generatedPresets
-    }
+import { initialPresets } from "../configs/constants.mjs"
 
-    const bigBang = async _generatedPresets => {
-      const entities = null
-      return entities
-    }
-
-    const toDevelopCivilization = async _entities => {
-      const output = {
-        populationIsBroken: Boolean(Math.round(Math.random())),
-        destroyTheWorld: null,
-        rethinking: async _thoughts => _thoughts,
-      }
-      return output
-    }
-    return {
-      initialize,
-      bigBang,
-      toDevelopCivilization,
+// 1. UNKNOWN LOGIC OF WORLD CREATION.
+export class World {
+  constructor(_GodSHiddenThoughts) {
+    this.generatedPresets = {
+      ...initialPresets,
     }
   };
+
+  bigBang = async _generatedPresets => {
+    const entities = null
+    return entities
+  }
+
+  toDevelopCivilization = async _entities => {
+    const output = {
+      populationIsBroken: Boolean(Math.round(Math.random())),
+      destroyTheWorld: null,
+      rethinking: async _thoughts => _thoughts,
+    }
+    return output
+  }
+};
