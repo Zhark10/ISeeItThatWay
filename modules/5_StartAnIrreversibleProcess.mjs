86bf5a3d531bf5ad6ce3a7c753eb046c6709bdd1
Zhark10 <verygoodman18@yandex.ru>
2022-04-14 19:39:53 +0300

    min web changes

--- modules/5_StartAnIrreversibleProcess.mjs
+++ modules/5_StartAnIrreversibleProcess.mjs
@@ -6,5 +6,7 @@ import {startNewSimulations} from './3_StartNewSimulations.mjs'
   const simulationsInfo = await startNewSimulations(GodSThoughts);
   const result = await GodSThoughts.analyze(simulationsInfo);
   // const mainAnswer = result.getAnswer('WHERE I AM?');
-  console.log(JSON.stringify(result, null, 2));
+  const formattedResult = JSON.stringify(result, null, 2)
+  console.log(result);
+  window.document.write(formattedResult);
 })()
